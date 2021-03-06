5397b3e43ba4c3157403e260793de9ccad607490
thuongnguyen <thuongnguyen1903@hotmail.com>
2021-03-06 23:26:49 +0700

    text

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
 var db = require('./db');
+var authMiddleware = require('./middlewares/auth.middlewares');
 const port = 8081;
 
 const app = express();
 app.use(bodyParser.json()) ;
-app.use(bodyParser.urlencoded({ extended: true })) ;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
+app.use(cookieParser());
 
 app.set('view engine','pug');
 app.set('views','./views');
@@ -19,6 +23,7 @@ app.get('/',(req,res) => {
       	name:'Thuong'
       });
 });
-app.use('/users',userRoute);
+app.use('/users',authMiddleware.requireAuth,userRoute);
+app.use('/auth',authRoute);
 
 app.listen(port,()=> console.log(`server listen in port:${port}`));
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,28 @@
 var express = require('express');
 
 var controller = require('../controllers/user.controller');
+
 var db = require('../db');
+
+var validate = require('../validate/user.validate');
+
+var authMiddleware = require('../middlewares/auth.middlewares');
+
 var router = express.Router();
+
 router.get('/',controller.index);
 
+router.get('/cookie',function(req,res,next){
+   res.cookie('user-id',123456);
+   res.send('Hello');
+});
+
 router.get('/search',controller.search);
+
 router.get('/create',controller.create);
 
 router.get('/:id',controller.get);
-router.post('/create',controller.postCreate);
+
+router.post('/create',validate.postCreate,controller.postCreate);
+
 module.exports = router;
